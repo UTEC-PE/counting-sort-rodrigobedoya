ce8405b4a0f1598e778304e47eece4d8aa76c028
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2018-09-19 11:53:30 -0500

    implemented counting sort algorithm

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     countingSort(numbers, size);
     print(numbers, size);
     
-    system("read");
+    //system("read");
     return EXIT_SUCCESS;
 }
 
@@ -26,5 +26,39 @@ void print(int numbers[], int size) {
 }
 
 void countingSort(int numbers[], int size) {
-    // TODO
+    int max = 0;
+    for(int i = 0; i < size;i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+
+    int counter[max+1] = {0};
+    {
+        for(int i = 0; i < size;i++)
+        {
+            counter[numbers[i]] += 1;
+        }
+        for(int i = 0; i < max;i++)
+        {
+            counter[i+1] += counter[i];
+        }
+    }
+
+    int temp[size] = {};
+    for(int i = 0; i < size; i++)
+    {
+        if(counter[numbers[i]] == 0)
+            continue;
+
+        temp[counter[numbers[i]]-1] = numbers[i];
+        counter[numbers[i]] -= 1;
+    }
+    for(int i = 0; i < size; i++)
+    {
+        cout << temp[i] << " ";
+    }
+    cout << endl;
 }
